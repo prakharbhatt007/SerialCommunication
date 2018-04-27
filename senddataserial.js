fbd9f15da99ba8dcc28b402ff46556744776c16d
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-04-27 14:46:04 +0530

    Showing the data recieved on console and send the data you typed on console serially

--- senddataserial.js
+++ senddataserial.js
@@ -1,8 +1,17 @@
 var SerialPort = require('serialport')
-var port = new SerialPort('/dev/ttyUSB0')
-port.write('Send data serially',function(err){
+var prompt = require('prompt')
+var port = new SerialPort('/dev/ttyUSB0',{baudRate: 115200})
+prompt.get(['data'],function(err,result){
 	if(err){
-		return console.log('Error',err.message)
+		console.log(err)
 	}
-	console.log('Data sucessfully sent Serially')
+	port.write(result.data,function(err){
+		if(err){
+			return console.log('Error'+err.message)
+		}
+	})
+})
+port.on('readable',function(err){
+	if(err)console.log(err)
+	console.log(''+port.read())
 })
